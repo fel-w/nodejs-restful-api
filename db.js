66f605c98588f71de19e514f3c10aefd41225cd0
fel-w <wangottafelix@gmail.com>
2022-11-08 08:47:40 +0300

    Connect to mongo db

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://felix:P8ckMwkS8R4uQL8D@cluster0.qekwdhx.mongodb.net/?retryWrites=true&w=majority');
