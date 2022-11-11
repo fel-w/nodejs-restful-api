347e8434b83fe793c1b423b40a9c1fab4ef0cd97
fel-w <wangottafelix@gmail.com>
2022-11-11 12:12:42 +0300

    Connect to local db

--- db.js
+++ db.js
@@ -1,2 +1,6 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://felix:P8ckMwkS8R4uQL8D@cluster0.qekwdhx.mongodb.net/?retryWrites=true&w=majority');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
