f1edc123c45129f1f58134ca87d808e3b26c6f3f
fel-w <wangottafelix@gmail.com>
2022-11-12 11:58:28 +0300

    Add token verification middleware

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -7,6 +7,7 @@ router.use(bodyParser.json());
 var User = require("../user/User");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
+var VerifyToken = require('./VerifyToken');
 
 router.post("/register", (req, res) => {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -33,30 +34,15 @@ router.post("/register", (req, res) => {
   );
 });
 
-router.get("/me", (req, res) => {
-  // token is expected along with in request header
-  var token = req.headers["x-access-token"];
-  if (!token)
-    return res
-      .status(401)
-      .send({ auth: false, message: "No token provided." });
-
-  // Decode token to view original payload
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err)
-      return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate token." });
-    
+router.get("/me", VerifyToken, (req, res) => {
     // Look up user with decoded token
-    User.findById(decoded.id, { password: 0 }, (err, user) => {
+    User.findById(req.userId, { password: 0 }, (err, user) => {
       if (err) return res.status(500).send("There was a problem finding the user");
       if (!user) return res.status(404).send("There was no user found");
 
       res.status(200).send(user);
     });
   });
-});
 
 // login user
 router.post('/login', (req, res) => {
